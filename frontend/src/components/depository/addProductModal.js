64cadb745e394e432a7c48da4c198d54fe472a94
Dier. Cohen <i.am.dier.cohen@gmail.com>
2019-12-10 16:01:43 +0330

    complete table

--- frontend/src/components/depository/addProductModal.js
+++ frontend/src/components/depository/addProductModal.js
@@ -2,74 +2,137 @@
 import React from "react";
 import { connect } from "react-redux";
 import {setNewBill,getActiveBill} from '../../actions/SaleActions'
-import { Button, Modal, Divider, Header, Segment, Form, Card, Popup, Icon, Message } from 'semantic-ui-react'
+import { Button, Modal, Divider, Dropdown, Segment, Form, Card, Popup, Icon, Message } from 'semantic-ui-react'
 import {toastr} from 'react-redux-toastr'
+import { getProductFields,setNewProduct } from '../../actions/DepositoryActions'
+const FORM_VALIDATION = {
+    code: false,
+    name: false,
+    buying_price: false,
+    selling_price: false,
+    stock_amount: false,
+    //--------- Options ---------
+    f_type: false,
+    design: false,
+    material: false,
+    background_color: false,
+    design_color: false,
+};
 class AddBillModal extends React.Component {
     constructor(props) {
         super(props);
     }
     componentDidMount() {
         // toastr.success('asdf','sadfsdfdfssdf')
-        this.props.getActiveBill();
+        this.props.getProductFields().then(() => {
+            console.log('propseeeee',this.props);
+            this.setState({
+                background_color_Options: this.props.productFields.background_color,
+                design_color_Options: this.props.productFields.design_color,
+                material_Options: this.props.productFields.material,
+                design_Options: this.props.productFields.design,
+                f_type_Options: this.props.productFields.f_type
+            })
+        });
     }
     state = {
-        isOpenAddItem: false,
-        formValidation: {
-            phone_number: false,
-            used_points: false,
-            branch: false,
-            discount: false,
-            items:false
-        },
-        phone_number: '',
-        used_points: '',
-        branch: 1,
-        discount: 0,
-        itemsDOM: [],
-        itemsDataSheet: [],
+        f_type: '',
+        design: '',
+        material:'',
+        background_color:'',
+        design_color: '',
+        //------------------------------
+        f_type_Options: {},
+        design_Options: {},
+        material_Options:{},
+        background_color_Options:{},
+        design_color_Options:{},
+        formValidation: FORM_VALIDATION,
+        code:'',
+        name:'',
+        buying_price:'',
+        selling_price:'',
+        stock_amount: '',
+        branch:1,
         branchOptions: [
             { key: '1', value: '1', flag: 'ir', text: 'شعبه یک' },
         ]
     };
     
-    toggleAddItemPopup = () => {
-        this.setState((prevState)=>({isOpenAddItem: !prevState.isOpenAddItem}))
-    }
     inputChange = (event, inputName) => {
-        console.log(event,inputName)
         this.setState({
             [inputName]: event.target.value
-        })
+        });
+    }
+    selectChange = (e, { name, value }) => {
+        this.setState({
+            [name]: value
+        },()=>{
+            console.log(this.state);
+        });
+        
     }
     formSubmitHandler = () => {
-        this.setState({ formValidation: { ...this.state.formValidation, used_points: false, discount: false, phone_number: false,items:false } }, () => {
+        this.setState({ formValidation: { ...this.state.formValidation, ...FORM_VALIDATION } }, () => {
             let hasError = false;
-            if (this.state.phone_number.length !== 11) {
-                this.setState({ formValidation: { ...this.state.formValidation, phone_number: true } });
+            if (String(this.state.code).length < 1) {
+                this.setState({ formValidation: { ...this.state.formValidation, code: true } });
                 hasError = true;
             }
-            if (this.state.used_points.length < 1) {
-                this.setState({ formValidation: { ...this.state.formValidation, used_points: true } });
+            if (String(this.state.name).length < 1) {
+                this.setState({ formValidation: { ...this.state.formValidation, name: true } });
                 hasError = true;
             }
-            if (this.state.discount.length < 1) {
-                this.setState({ formValidation: { ...this.state.formValidation, discount: true } });
+            if (String(this.state.buying_price).length < 1) {
+                this.setState({ formValidation: { ...this.state.formValidation, buying_price: true } });
                 hasError = true;
             }
-            if (this.state.itemsDataSheet.length < 1) {
-                this.setState({ formValidation: { ...this.state.formValidation, items: true } });
+            if (String(this.state.selling_price).length < 1) {
+                this.setState({ formValidation: { ...this.state.formValidation, selling_price: true } });
+                hasError = true;
+            }
+            if (String(this.state.stock_amount).length < 1) {
+                this.setState({ formValidation: { ...this.state.formValidation, stock_amount: true } });
+                hasError = true;
+            }
+            // ---------- Options ----------
+            if (String(this.state.background_color).length < 1) {
+                this.setState({ formValidation: { ...this.state.formValidation, background_color: true } });
+                hasError = true;
+            }
+            if (String(this.state.design_color).length < 1) {
+                this.setState({ formValidation: { ...this.state.formValidation, design_color: true } });
+                hasError = true;
+            }
+            if (String(this.state.material).length < 1) {
+                this.setState({ formValidation: { ...this.state.formValidation, material: true } });
+                hasError = true;
+            }
+            if (String(this.state.f_type).length < 1) {
+                this.setState({ formValidation: { ...this.state.formValidation, f_type: true } });
+                hasError = true;
+            }
+            if (String(this.state.design).length < 1) {
+                this.setState({ formValidation: { ...this.state.formValidation, design: true } });
                 hasError = true;
             }
             if (!hasError) {
                 const prepareData = {
-                    phone_number: this.state.phone_number,
-                    discount: this.state.discount,
-                    used_points: this.state.used_points,
+                    code:this.state.code,
+                    name:this.state.name,
+                    buying_price:this.state.buying_price,
+                    selling_price:this.state.selling_price,
+                    stock_amount: this.state.stock_amount,
                     branch: this.state.branch,
-                    items: this.state.itemsDataSheet
+                    background_color: this.state.background_color,
+                    f_type: this.state.f_type,
+                    design_color: this.state.design_color,
+                    design: this.state.design,
+                    material: this.state.material,
+                    
                 }
-                console.log(prepareData, hasError);
-                this.props.setNewBill(prepareData);
+                console.log('prepareData',prepareData, hasError);
+                this.props.setNewProduct(prepareData);
             }
         });
         
@@ -100,10 +163,16 @@ class AddBillModal extends React.Component {
                                     <Form.Input className='ltr placeholder-rtl' label='مقدار باقی مانده' type="number" error={this.state.formValidation.stock_amount} onChange={(e)=>this.inputChange(e,'stock_amount')} placeholder='مقدار باقی مانده' />
                                     <Form.Dropdown className='ltr placeholder-rtl text-right' defaultValue={'1'} placeholder='شعبه' selection label='شعبه' options={this.state.branchOptions} />
                                 </Form.Group>
-                                <Form.Group widths={2}>
-                                    
-                                    {/* <Form.Input   placeholder='شعبه' /> */}
-                                    <Form.Input className='ltr placeholder-rtl' label='تخفیف کلی' type="number" defaultValue='0' error={this.state.formValidation.discount} onChange={(e)=>this.inputChange(e,'discount')} placeholder='مقدار تخفیف' />
+                                <Divider />
+                                
+                                <Form.Group widths={3}>
+                                    <Form.Select fluid placeholder='رنگ پس زمینه' label='رنگ پس زمینه' search selection error={this.state.formValidation.background_color} onChange={this.selectChange} name="background_color" options={this.state.background_color_Options} />
+                                    <Form.Select fluid placeholder='رنگ طرح' label='رنگ طرح' search selection error={this.state.formValidation.design_color} onChange={this.selectChange} name="design_color" options={this.state.design_color_Options} />
+                                    <Form.Select fluid placeholder='جنس' label='جنس' search selection error={this.state.formValidation.material} onChange={this.selectChange} name="material" options={this.state.material_Options} />
+                                </Form.Group>
+                                <Form.Group widths={3}>
+                                    <Form.Select fluid placeholder='نوع پارچه' label='نوع پارچه' search selection error={this.state.formValidation.f_type} onChange={this.selectChange} name="f_type" options={this.state.f_type_Options} />
+                                    <Form.Select fluid placeholder='نوع طرح' label='نوع طرح' search selection error={this.state.formValidation.design} onChange={this.selectChange} name="design" options={this.state.design_Options} />
                                 </Form.Group>
                                 
                             </Form>
@@ -116,7 +185,7 @@ class AddBillModal extends React.Component {
                             positive
                             icon='checkmark'
                             labelPosition='right'
-                            content="ثبت فاکتور"
+                            content="ثبت محصول"
                             onClick={this.formSubmitHandler}
                         />
                     </Modal.Actions>
@@ -128,9 +197,9 @@ class AddBillModal extends React.Component {
 }
 
 const mapStateToProps = state => {
-    console.log(state)
-//   return {
-//     nextReceipt: state.receipts.nextReceipt,
+    // console.log('state',state)
+  return {
+        productFields: state.depository.productFields,
 //     currentUser: state.auth.currentUser
 //       ? state.auth.currentUser
 //       : localStorage.getItem("user")
@@ -141,10 +210,10 @@ const mapStateToProps = state => {
 //       : localStorage.getItem("type")
 //       ? localStorage.getItem("type")
 //       : ""
-//   };
+  };
 };
 
 export default connect(
   mapStateToProps,
-  { setNewBill,getActiveBill }
+  { getProductFields,setNewProduct }
 )(AddBillModal);
--- frontend/src/components/depository/productTable.js
+++ frontend/src/components/depository/productTable.js
@@ -1,27 +1,37 @@
 import React from "react";
 import { connect } from "react-redux";
-import { Table,Pagination } from 'semantic-ui-react'
-import { getProductFields } from '../../actions/DepositoryActions'
+import { Table, Pagination } from 'semantic-ui-react'
+import { getProductsList } from '../../actions/DepositoryActions'
+
 class ProductTable extends React.Component {
     constructor(props) {
         super(props);
     }
     state = {
+        productsList:[]
     }
     componentDidMount() {
-        this.props.getProductFields();
-        console.log(this.props)
+        this.props.getProductsList().then(() => {
+            console.log('xlksdjflksdkljflksdjflkjsdflksd', this.props);
+            this.setState({productsList:this.props.productsList.results})
+        })
     }
     render() {
         return (
         <Table celled striped className="">
             <Table.Header>
             <Table.Row>
-                <Table.HeaderCell colSpan='4' className="rtl text-right">لیست محصولات</Table.HeaderCell>
+                <Table.HeaderCell colSpan='10' className="rtl text-right">لیست محصولات</Table.HeaderCell>
                     </Table.Row>
                     <Table.Row>
-                    <Table.HeaderCell className="text-center">عملیات</Table.HeaderCell>
+                    <Table.HeaderCell className="text-center">رنگ پس زمینه</Table.HeaderCell>
+                    <Table.HeaderCell className="text-center">رنگ طرح</Table.HeaderCell>
+                    <Table.HeaderCell className="text-center">جنس</Table.HeaderCell>
+                    <Table.HeaderCell className="text-center">نوع پارچه</Table.HeaderCell>
+                    <Table.HeaderCell className="text-center">نوع طرح</Table.HeaderCell>
                     <Table.HeaderCell className="text-center">مقدار باقی مانده</Table.HeaderCell>
+                    <Table.HeaderCell className="text-center">قیمت فروش</Table.HeaderCell>
+                    <Table.HeaderCell className="text-center">قیمت خرید</Table.HeaderCell>
                     <Table.HeaderCell className="text-center">نام محصول</Table.HeaderCell>
                     <Table.HeaderCell className="text-center">کد محصول</Table.HeaderCell>
       </Table.Row>
@@ -29,24 +39,30 @@ class ProductTable extends React.Component {
 
                 <Table.Body>
                     
-            <Table.Row>
-                <Table.Cell>
-                node_modules
-                </Table.Cell>
-                <Table.Cell >
-                node_modules
-                </Table.Cell>
-                <Table.Cell collapsing>Initial commit</Table.Cell>
-                <Table.Cell collapsing textAlign='right'>
-                10 hours ago
-                </Table.Cell>
-            </Table.Row>
+                        {this.state.productsList.map(item => {
+                            return(<Table.Row>
+                                    <Table.Cell className="norm-latin text-center"><span className="yekan">{item.background_color.name}</span></Table.Cell>
+                                    <Table.Cell className="norm-latin text-center"><span className="yekan">{item.design_color.name}</span></Table.Cell>
+                                    <Table.Cell className="norm-latin text-center"><span className="yekan">{item.material.name}</span></Table.Cell>
+                                    <Table.Cell className="norm-latin text-center"><span className="yekan">{item.f_type.name}</span></Table.Cell>
+                                    <Table.Cell className="norm-latin text-center"><span className="yekan">{item.design.name}</span></Table.Cell>
+                                    <Table.Cell className="norm-latin text-center rtl">
+                                    <span>{item.stock_amount}</span>
+                                    <span className="yekan">متر</span>
+                                    </Table.Cell>
+                                    <Table.Cell className="norm-latin text-center rtl"><span>{item.selling_price}</span> <span className="yekan">تومان</span></Table.Cell>
+                                    <Table.Cell className="norm-latin text-center rtl"><span>{item.buying_price}</span> <span className="yekan">تومان</span></Table.Cell>
+                                    <Table.Cell className="norm-latin text-center"><span>{item.code}</span></Table.Cell>
+                                    <Table.Cell className="norm-latin text-center" textAlign='right'>
+                                    <span>{item.code}</span>
+                                    </Table.Cell>
+                                </Table.Row>);
+                        })}
             
                 </Table.Body>
-
                 <Table.Footer fullWidth>
                 <Table.Row>
-                    <Table.HeaderCell colSpan='4' className="norm-latin">
+                    <Table.HeaderCell colSpan='10' className="norm-latin">
                       <Pagination className="norm-latin" defaultActivePage={5} totalPages={10} />
                     </Table.HeaderCell>
                 </Table.Row>
@@ -57,9 +73,8 @@ class ProductTable extends React.Component {
 }
 
 const mapStateToProps = state => {
-    console.log(state)
-//   return {
-//     nextReceipt: state.receipts.nextReceipt,
+  return {
+            productsList: state.depository.productsList,
 //     currentUser: state.auth.currentUser
 //       ? state.auth.currentUser
 //       : localStorage.getItem("user")
@@ -70,10 +85,10 @@ const mapStateToProps = state => {
 //       : localStorage.getItem("type")
 //       ? localStorage.getItem("type")
 //       : ""
-//   };
+  };
 };
 
 export default connect(
-    null,
-    {getProductFields}
+    mapStateToProps,
+    { getProductsList }
 )(ProductTable);
