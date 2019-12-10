9fd78db24759714f0684c4a4bfd7892125b50fdb
Sadjad's <sadjadtavakoli@gmail.com>
2019-12-10 10:52:23 +0330

    make class_type nullable

--- backend/customer/models.py
+++ backend/customer/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.conf import settings
 from django.db.models import DO_NOTHING
 from django.utils.translation import gettext_lazy as _
 
@@ -13,7 +12,8 @@ class Customer(models.Model):
     birth_date = models.DateField(null=True, blank=True)
     marriage_date = models.DateField(null=True, blank=True)
     points = models.IntegerField(default=0)
-    class_type = models.ForeignKey('customer.CustomerType', related_name='customers', on_delete=DO_NOTHING)
+    class_type = models.ForeignKey('customer.CustomerType', related_name='customers', on_delete=DO_NOTHING, null=True,
+                                   blank=True)
 
     def special_discount(self, value):
         discount = 0
