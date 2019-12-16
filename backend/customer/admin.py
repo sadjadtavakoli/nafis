5ccc75c241ab43b1e6c705a4770e3a82496bbde3
Sadjad's <sadjadtavakoli@gmail.com>
2019-12-16 22:37:33 +0330

    add point to admin

--- backend/customer/admin.py
+++ backend/customer/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
-from customer.models import Customer
+from customer.models import Customer, Point, CustomerType
 
 admin.site.register(Customer)
+admin.site.register(CustomerType)
+admin.site.register(Point)
