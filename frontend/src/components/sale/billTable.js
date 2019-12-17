01d6a4092bb50934e30f4d5add662646cc5469aa
Dier. Cohen <i.am.dier.cohen@gmail.com>
2019-12-17 15:00:39 +0330

    factor page

--- frontend/src/components/sale/billTable.js
+++ frontend/src/components/sale/billTable.js
@@ -6,6 +6,7 @@ import { digitToComma, phoneNumberBeautifier } from '../utils/numberUtils';
 import { standardTimeToJalaali } from '../utils/jalaaliUtils'
 import ShowInformationModal from './showInformationModal'
 import LoadingBar from '../utils/loadingBar'
+import NotFound from '../utils/notFound'
 import NewBillPopup from './newBillPopup'
 
 const colSpan = 7;
@@ -19,7 +20,8 @@ class BillTable extends React.Component {
         isOpenInformationModal: false,
         itemData: {},
         isOpenAddItem: NaN,
-        activePage:1
+        activePage: 1,
+        firstTime:true
     }
     componentWillReceiveProps(newProps) {
         if ( newProps.newBillData && (newProps.newBillData.pk !== this.props.newBillData.pk) ) {
@@ -33,6 +35,7 @@ class BillTable extends React.Component {
     getActiveBill = (page = 1) => {
         this.props.getActiveBill(page).then(() => {
             this.setState({
+                firstTime: false,
                 activeBill: this.props.activeBill ? this.props.activeBill.results : {},
                 totalPageCount: this.props.activeBill? Math.ceil(this.props.activeBill.count / 25):0,
             });
@@ -68,77 +71,84 @@ class BillTable extends React.Component {
         this.toggleAddItemPopup();
     };
     render() {
-        return this.state.activeBill.length > 0 ? (
-            <>
-        <ShowInformationModal data={this.state.itemData} open={this.state.isOpenInformationModal} onClose={this.closeInformationModal}/>
+        if (this.state.activeBill.length === 0 && !this.state.firstTime) {
+            return <NotFound />
+        }
+        if (this.state.activeBill.length > 0){
+            return (
+                <>
+                    <ShowInformationModal data={this.state.itemData} open={this.state.isOpenInformationModal} onClose={this.closeInformationModal} />
                 
-        <Table celled striped className="">
-            <Table.Header>
-            <Table.Row>
-                <Table.HeaderCell colSpan={colSpan} className="rtl text-right">لیست فاکتور های فعال</Table.HeaderCell>
-                    </Table.Row>
-                    <Table.Row>
-                    <Table.HeaderCell className="text-center">عملیات</Table.HeaderCell>
-                    <Table.HeaderCell className="text-center">تعداد پرداختی ها</Table.HeaderCell>
-                    <Table.HeaderCell className="text-center">تاریخ ثبت</Table.HeaderCell>
-                    <Table.HeaderCell className="text-center">مبلغ کل</Table.HeaderCell>
-                    <Table.HeaderCell className="text-center">مبلغ نهایی</Table.HeaderCell>
-                    <Table.HeaderCell className="text-center">تخفیف کل</Table.HeaderCell>
-                    <Table.HeaderCell className="text-center">شماره تلفن خریدار</Table.HeaderCell>
-            </Table.Row>
-            </Table.Header>
+                    <Table celled striped className="">
+                        <Table.Header>
+                            <Table.Row>
+                                <Table.HeaderCell colSpan={colSpan} className="rtl text-right">لیست فاکتور های فعال</Table.HeaderCell>
+                            </Table.Row>
+                            <Table.Row>
+                                <Table.HeaderCell className="text-center">عملیات</Table.HeaderCell>
+                                <Table.HeaderCell className="text-center">تعداد پرداختی ها</Table.HeaderCell>
+                                <Table.HeaderCell className="text-center">تاریخ ثبت</Table.HeaderCell>
+                                <Table.HeaderCell className="text-center">مبلغ کل</Table.HeaderCell>
+                                <Table.HeaderCell className="text-center">مبلغ نهایی</Table.HeaderCell>
+                                <Table.HeaderCell className="text-center">تخفیف کل</Table.HeaderCell>
+                                <Table.HeaderCell className="text-center">شماره تلفن خریدار</Table.HeaderCell>
+                            </Table.Row>
+                        </Table.Header>
 
-                <Table.Body>
-                     {/* <Input
+                        <Table.Body>
+                            {/* <Input
                         iconPosition='left'
                         icon={<Icon name='search' inverted circular link />}
                         placeholder='Search...'
                     /> */}
                            
-                        {this.state.activeBill.map((item,index) => {
-                            return (
-                                <>
-                                <Table.Row key={index}>
-                                        <Table.Cell className="norm-latin text-center">
-                                            <Popup
-                                            content={<NewBillPopup refetch={()=>this.getActiveBill(this.state.activePage)} phoneNumber={item.buyer.phone_number} pk={item.pk} onClose={()=>this.toggleAddItemPopup(NaN)} onSubmit={this.submitItemPopup}/>}
-                                            open={this.state.isOpenAddItem === index}
-                                            className="no-filter"
-                                            position='bottom center'
-                                            wide='very'
-                                                trigger={
-                                                     <Button onClick={()=>this.toggleAddItemPopup(index)} icon labelPosition='right' color="green">
-                                                        <span className="yekan" >افزودن آیتم جدید</span>
-                                                        <Icon name='add' />
-                                                    </Button>
-                                                }
-                                                color='green' size='huge' icon='add' />
+                            {this.state.activeBill.map((item, index) => {
+                                return (
+                                    <>
+                                        <Table.Row key={index}>
+                                            <Table.Cell className="norm-latin text-center">
+                                                <Popup
+                                                    content={<NewBillPopup refetch={() => this.getActiveBill(this.state.activePage)} phoneNumber={item.buyer.phone_number} pk={item.pk} onClose={() => this.toggleAddItemPopup(NaN)} onSubmit={this.submitItemPopup} />}
+                                                    open={this.state.isOpenAddItem === index}
+                                                    className="no-filter"
+                                                    position='bottom center'
+                                                    wide='very'
+                                                    trigger={
+                                                        <Button onClick={() => this.toggleAddItemPopup(index)} icon labelPosition='right' color="green">
+                                                            <span className="yekan" >افزودن آیتم جدید</span>
+                                                            <Icon name='add' />
+                                                        </Button>
+                                                    }
+                                                    color='green' size='huge' icon='add' />
                            
-                                    <Button onClick={()=>this.openInformationModal(item)} icon labelPosition='right' color="yellow">
-                                       <span className="yekan" >نمایش اطلاعات</span>
-                                        <Icon name='info' />
-                                    </Button>
-                                    </Table.Cell>
-                                    <Table.Cell className="norm-latin text-center rtl"><span >{item.payments.length}</span>&nbsp;<span className="yekan">پرداختی</span></Table.Cell>
-                                    <Table.Cell className="norm-latin text-center"><span>{standardTimeToJalaali(item.create_date)}</span></Table.Cell>
-                                    <Table.Cell className="norm-latin text-center rtl"><span>{digitToComma(item.price)}</span>&nbsp;<span className="yekan">تومان</span></Table.Cell>
-                                    <Table.Cell className="norm-latin text-center rtl"><span>{digitToComma(item.final_price)}</span>&nbsp;<span className="yekan">تومان</span></Table.Cell>
-                                    <Table.Cell className="norm-latin text-center">{item.final_discount?(<><span className="yekan">digitToComma(item.final_discount)</span><span className="yekan">تومان</span></>):'--'}  </Table.Cell>
-                                    <Table.Cell className="norm-latin text-center"><span>{phoneNumberBeautifier(item.buyer.phone_number)}</span></Table.Cell>
-                                    </Table.Row>
-                                </>);
+                                                <Button onClick={() => this.openInformationModal(item)} icon labelPosition='right' color="yellow">
+                                                    <span className="yekan" >نمایش اطلاعات</span>
+                                                    <Icon name='info' />
+                                                </Button>
+                                            </Table.Cell>
+                                            <Table.Cell className="norm-latin text-center rtl"><span >{item.payments.length}</span>&nbsp;<span className="yekan">پرداختی</span></Table.Cell>
+                                            <Table.Cell className="norm-latin text-center"><span>{standardTimeToJalaali(item.create_date)}</span></Table.Cell>
+                                            <Table.Cell className="norm-latin text-center rtl"><span>{digitToComma(item.price)}</span>&nbsp;<span className="yekan">تومان</span></Table.Cell>
+                                            <Table.Cell className="norm-latin text-center rtl"><span>{digitToComma(item.final_price)}</span>&nbsp;<span className="yekan">تومان</span></Table.Cell>
+                                            <Table.Cell className="norm-latin text-center">{item.final_discount ? (<><span className="yekan">digitToComma(item.final_discount)</span><span className="yekan">تومان</span></>) : '--'}  </Table.Cell>
+                                            <Table.Cell className="norm-latin text-center"><span>{phoneNumberBeautifier(item.buyer.phone_number)}</span></Table.Cell>
+                                        </Table.Row>
+                                    </>);
                             })}
                 
-                </Table.Body>
-                <Table.Footer fullWidth hidden={this.state.totalPageCount < 2}>
-                <Table.Row>
-                    <Table.HeaderCell colSpan={colSpan} className="norm-latin">
-                      <Pagination className="norm-latin" defaultActivePage={1} onPageChange={this.changePage} totalPages={this.state.totalPageCount} />
-                    </Table.HeaderCell>
-                </Table.Row>
-                </Table.Footer>
-                </Table>
-            </>) : <LoadingBar/>;
+                        </Table.Body>
+                        <Table.Footer fullWidth hidden={this.state.totalPageCount < 2}>
+                            <Table.Row>
+                                <Table.HeaderCell colSpan={colSpan} className="norm-latin">
+                                    <Pagination className="norm-latin" defaultActivePage={1} onPageChange={this.changePage} totalPages={this.state.totalPageCount} />
+                                </Table.HeaderCell>
+                            </Table.Row>
+                        </Table.Footer>
+                    </Table>
+                </>)
+    }else{
+    return <LoadingBar />;
+    }
     }
                                         
 }
