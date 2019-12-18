a5ce435dfd5ef3f16abd60a25727842ff1e088f9
Dier. Cohen <i.am.dier.cohen@gmail.com>
2019-12-18 11:44:42 +0330

    add server ip

--- frontend/src/apis/auth.js
+++ frontend/src/apis/auth.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const PROTOCOL = 'http';
-const BASE = '127.0.0.1';
+const BASE = '194.5.175.63';
 const PORT = 8000;
 const auth = axios.create({
     baseURL: `${PROTOCOL}://${BASE}:${PORT}/login/`
