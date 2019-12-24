d517b6810bbfad588bc69f1d50b3eb85c4b7adde
Dier. Cohen <i.am.dier.cohen@gmail.com>
2019-12-25 00:57:51 +0330

    all total bill

--- frontend/src/components/cashRegister/ProductItems.js
+++ frontend/src/components/cashRegister/ProductItems.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Header, Label } from "semantic-ui-react";
+import { Header } from "semantic-ui-react";
 import RenderItem from "./RenderItem";
 import {enToFa,priceToPersian} from '../utils/numberUtils'
 const ProductItems = props => {
@@ -7,9 +7,6 @@ const ProductItems = props => {
     <React.Fragment>
       <Header as="h3" textAlign="right">
           <span>اقلام: </span>
-        <Label color="teal">
-          <span>تخفیف کلی اقلام: </span><span>{enToFa(priceToPersian(props.data.total_discount))} تومان</span>
-        </Label>
       </Header>
       {props.data.items.map((productItem, index) => (
         <RenderItem productItem={productItem} index={index} />
