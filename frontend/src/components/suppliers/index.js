f38650a557165f1434948183981737a10877b21a
incdious <incdious@gmail.com>
2020-01-15 12:04:54 +0330

    making the suppliers table

--- frontend/src/components/suppliers/index.js
+++ frontend/src/components/suppliers/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { getSuppliersAction } from "../../actions/SuppliersActions";
+import { Table, Grid, Search, Button, Pagination } from "semantic-ui-react";
 
 class Suppliers extends Component {
   componentDidMount() {
@@ -14,7 +15,73 @@ class Suppliers extends Component {
   render() {
     return (
       <div>
-        <h1>suppliers</h1>
+        <React.Fragment>
+          <Table celled className="rtl text-center" columns={3}>
+            <Table.Header className="text-right">
+              <Table.Row>
+                <Table.HeaderCell colSpan="4">
+                  <Grid columns={1}>
+                    <Grid.Row className="us-header">
+                      <span className="us-p us-users">تامین کنندگان</span>
+                      <Search
+                        id="us-search"
+                        showNoResults={false}
+                        placeholder="جست و جو..."
+                        className="placeholder-rtl yekan ltr"
+                      />
+                    </Grid.Row>
+                  </Grid>
+                </Table.HeaderCell>
+              </Table.Row>
+            </Table.Header>
+
+            <Table.Header>
+              <Table.Row>
+                <Table.HeaderCell style={{ borderLeft: "1px solid #ddd" }}>
+                  نام و نام خانوادگی
+                </Table.HeaderCell>
+                <Table.HeaderCell>شماره موبایل</Table.HeaderCell>
+                <Table.HeaderCell>عملیات</Table.HeaderCell>
+              </Table.Row>
+            </Table.Header>
+
+            <Table.Body>
+              <Table.Row>
+                <Table.Cell style={{ borderLeft: "1px solid #ddd" }}>
+                  first name last name
+                </Table.Cell>
+                <Table.Cell className="norm-latin">
+                  <span>phone number</span>
+                </Table.Cell>
+                <Table.Cell>
+                  <Button color="teal">
+                    <span>مشاهده</span>
+                  </Button>
+                </Table.Cell>
+              </Table.Row>
+            </Table.Body>
+
+            {this.props.usersCustomers &&
+            this.props.usersCustomers.count > 25 ? (
+              <Table.Footer>
+                <Table.Row>
+                  <Table.HeaderCell colSpan="3">
+                    <Pagination
+                      className="norm-latin ltr"
+                      defaultActivePage={1}
+                      onPageChange={this.changePage}
+                      firstItem={null}
+                      lastItem={null}
+                      totalPages={Math.ceil(
+                        this.props.usersCustomers.count / 25
+                      )}
+                    />
+                  </Table.HeaderCell>
+                </Table.Row>
+              </Table.Footer>
+            ) : null}
+          </Table>
+        </React.Fragment>
       </div>
     );
   }
