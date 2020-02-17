9cdc3cc32f81b7d40bdd71c7255e0309ae6f6254
Dier. Cohen <i.am.dier.cohen@gmail.com>
2020-02-17 17:53:09 +0330

    reback xx

--- frontend/src/components/depository/productTable.js
+++ frontend/src/components/depository/productTable.js
@@ -15,6 +15,7 @@ import {
 import { digitToComma } from "../utils/numberUtils";
 import LoadingBar from "../utils/loadingBar";
 import NotFound from "../utils/notFound";
+import history from "../../history";
 
 class ProductTable extends React.Component {
   state = {
@@ -22,13 +23,16 @@ class ProductTable extends React.Component {
     totalPageCount: 1,
     activePage: 1,
     notFound: false,
-    width: 0
+    width: 0,
+    customers: []
   };
+
   componentDidMount() {
     this.getProductsList();
     this.updateWindowDimensions();
     window.addEventListener("resize", this.updateWindowDimensions);
   }
+
   componentWillReceiveProps(newProps) {
     if (newProps.productsList && newProps.productsList.results) {
       this.setState({
@@ -37,29 +41,32 @@ class ProductTable extends React.Component {
       });
     }
   }
+
   componentWillUnmount() {
     window.removeEventListener("resize", this.updateWindowDimensions);
   }
+
   getProductsList = (page = 1) => {
     this.props.getProductsList(page).then(() => {
       this.setState({
         notFound: false,
-        productsList: this.props.productsList
-          ? this.props.productsList.results
-          : {},
+        productsList: this.props.productsList.results,
         totalPageCount: this.props.productsList
           ? Math.ceil(this.props.productsList.count / 25)
           : 1
       });
     });
   };
+
   updateWindowDimensions = () => {
     this.setState({ width: window.innerWidth });
   };
-  changePage = (event, { activePage }) => {
+
+  changePage = (_, { activePage }) => {
     this.setState({ activePage });
     this.getProductsList(activePage);
   };
+
   handleSearchChange = (e, { value }) => {
     this.setState({ searchLoading: true, value });
 
@@ -85,6 +92,7 @@ class ProductTable extends React.Component {
       });
     }, 300);
   };
+
   searchBar = () => {
     return (
       <Search
@@ -98,11 +106,12 @@ class ProductTable extends React.Component {
       />
     );
   };
+
   render() {
     return (
       <div>
         {this.state.productsList.length > 0 ? (
-          <Table celled striped className="">
+          <Table celled striped>
             <Table.Header>
               <Table.Row>
                 <Table.HeaderCell colSpan="10" className="rtl text-right">
@@ -132,95 +141,99 @@ class ProductTable extends React.Component {
                 </Table.HeaderCell>
               </Table.Row>
               <Table.Row>
-                {!this.state.notFound ? (
-                  <React.Fragment>
-                    <Table.HeaderCell className="text-center">
-                      رنگ پس زمینه
-                    </Table.HeaderCell>
-                    <Table.HeaderCell className="text-center">
-                      رنگ طرح
-                    </Table.HeaderCell>
-                    <Table.HeaderCell className="text-center">
-                      جنس
-                    </Table.HeaderCell>
-                    <Table.HeaderCell className="text-center">
-                      نوع پارچه
-                    </Table.HeaderCell>
-                    <Table.HeaderCell className="text-center">
-                      نوع طرح
-                    </Table.HeaderCell>
-                    <Table.HeaderCell className="text-center">
-                      مقدار باقی مانده
-                    </Table.HeaderCell>
-                    <Table.HeaderCell className="text-center">
-                      قیمت فروش
-                    </Table.HeaderCell>
-                    <Table.HeaderCell className="text-center">
-                      نام محصول
-                    </Table.HeaderCell>
-                    <Table.HeaderCell className="text-center">
-                      کد محصول
-                    </Table.HeaderCell>
-                  </React.Fragment>
-                ) : (
-                  <NotFound />
-                )}
+                <Table.HeaderCell className="text-center">
+                  عملیات
+                </Table.HeaderCell>
+                <Table.HeaderCell className="text-center">
+                  رنگ پس زمینه
+                </Table.HeaderCell>
+                <Table.HeaderCell className="text-center">
+                  رنگ طرح
+                </Table.HeaderCell>
+                <Table.HeaderCell className="text-center">جنس</Table.HeaderCell>
+                <Table.HeaderCell className="text-center">
+                  نوع پارچه
+                </Table.HeaderCell>
+                <Table.HeaderCell className="text-center">
+                  نوع طرح
+                </Table.HeaderCell>
+                <Table.HeaderCell className="text-center">
+                  مقدار باقی مانده
+                </Table.HeaderCell>
+                <Table.HeaderCell className="text-center">
+                  قیمت فروش
+                </Table.HeaderCell>
+                <Table.HeaderCell className="text-center">
+                  نام محصول
+                </Table.HeaderCell>
+                <Table.HeaderCell className="text-center">
+                  کد محصول
+                </Table.HeaderCell>
               </Table.Row>
             </Table.Header>
 
             <Table.Body>
-              {!this.state.notFound
-                ? this.state.productsList.map((item, index) => {
-                    return (
-                      <Table.Row key={index}>
-                        <Table.Cell className="norm-latin text-center">
-                          <span className="yekan">
-                            {item.background_color &&
-                              item.background_color.name}
-                          </span>
-                        </Table.Cell>
-                        <Table.Cell className="norm-latin text-center">
-                          <span className="yekan">
-                            {item.design_color && item.design_color.name}
-                          </span>
-                        </Table.Cell>
-                        <Table.Cell className="norm-latin text-center">
-                          <span className="yekan">
-                            {item.material && item.material.name}
-                          </span>
-                        </Table.Cell>
-                        <Table.Cell className="norm-latin text-center">
-                          <span className="yekan">
-                            {item.f_type && item.f_type.name}
-                          </span>
-                        </Table.Cell>
-                        <Table.Cell className="norm-latin text-center">
-                          <span className="yekan">
-                            {item.design && item.design.name}
-                          </span>
-                        </Table.Cell>
-                        <Table.Cell className="norm-latin text-center ltr">
-                          <span className="yekan">متر</span>
-                          <span>&nbsp;</span>
-                          <span>{item.stock_amount}</span>
-                        </Table.Cell>
-                        <Table.Cell className="norm-latin text-center rtl">
-                          <span>{digitToComma(item.selling_price)}</span>{" "}
-                          <span className="yekan">تومان</span>
-                        </Table.Cell>
-                        <Table.Cell className="yekan text-center">
-                          <span>{item.name}</span>
-                        </Table.Cell>
-                        <Table.Cell
-                          className="norm-latin text-center"
-                          textAlign="right"
-                        >
-                          <span>{item.code}</span>
-                        </Table.Cell>
-                      </Table.Row>
-                    );
-                  })
-                : null}
+              {this.state.productsList.map((item, index) => {
+                return (
+                  <Table.Row key={index}>
+                    <Table.Cell className="text-center">
+                      <Button
+                        color="teal"
+                        onClick={() =>
+                          history.push(
+                            `/depository/depository-edit/${item.code}/${item.pk}/`
+                          )
+                        }
+                      >
+                        <span>ویرایش</span>
+                      </Button>
+                    </Table.Cell>
+                    <Table.Cell className="norm-latin text-center">
+                      <span className="yekan">
+                        {item.background_color && item.background_color.name}
+                      </span>
+                    </Table.Cell>
+                    <Table.Cell className="norm-latin text-center">
+                      <span className="yekan">
+                        {item.design_color && item.design_color.name}
+                      </span>
+                    </Table.Cell>
+                    <Table.Cell className="norm-latin text-center">
+                      <span className="yekan">
+                        {item.material && item.material.name}
+                      </span>
+                    </Table.Cell>
+                    <Table.Cell className="norm-latin text-center">
+                      <span className="yekan">
+                        {item.f_type && item.f_type.name}
+                      </span>
+                    </Table.Cell>
+                    <Table.Cell className="norm-latin text-center">
+                      <span className="yekan">
+                        {item.design && item.design.name}
+                      </span>
+                    </Table.Cell>
+                    <Table.Cell className="norm-latin text-center ltr">
+                      <span className="yekan">متر</span>
+                      <span>&nbsp;</span>
+                      <span>{item.stock_amount}</span>
+                    </Table.Cell>
+                    <Table.Cell className="norm-latin text-center rtl">
+                      <span>{digitToComma(item.selling_price)}</span>{" "}
+                      <span className="yekan">تومان</span>
+                    </Table.Cell>
+                    <Table.Cell className="yekan text-center">
+                      <span>{item.name}</span>
+                    </Table.Cell>
+                    <Table.Cell
+                      className="norm-latin text-center"
+                      textAlign="right"
+                    >
+                      <span>{item.code}</span>
+                    </Table.Cell>
+                  </Table.Row>
+                );
+              })}
             </Table.Body>
 
             <Table.Footer fullWidth hidden={this.state.totalPageCount < 2}>
@@ -251,12 +264,12 @@ class ProductTable extends React.Component {
 }
 
 const mapStateToProps = state => {
-  console.log(state.depository);
   return {
     productsList: state.depository.productsList
   };
 };
 
-export default connect(mapStateToProps, { getProductsList, getProductsByCode })(
-  ProductTable
-);
+export default connect(mapStateToProps, {
+  getProductsList,
+  getProductsByCode
+})(ProductTable);
