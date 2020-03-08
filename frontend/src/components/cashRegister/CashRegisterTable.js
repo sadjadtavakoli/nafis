2af70d384df9c38625408433cd3cf621cc3c7908
incdious <incdious@gmail.com>
2020-03-08 13:49:25 +0330

    INDEX, CASHREGISTERTABLE

--- frontend/src/components/cashRegister/CashRegisterTable.js
+++ frontend/src/components/cashRegister/CashRegisterTable.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { Table, Button } from "semantic-ui-react";
-import { connect } from "react-redux";
 import { getActiveBills, deleteBill } from "../../actions/CashRegisterActions";
 import { digitToComma } from "../utils/numberUtils";
 import { standardTimeToJalaali, convertToJalaali } from "../utils/jalaaliUtils";
@@ -9,150 +9,152 @@ import LoadingBar from "../utils/loadingBar";
 import { toastr } from "react-redux-toastr";
 import history from "../../history";
 import TableLabel from "../utils/tableLabelGenerator";
-class CashRegisterTable extends React.Component {
-  state = {
-    fetch: false,
-    noResult: false
-  };
 
-  componentDidMount() {
-    this.getActiveBills();
-  }
+const CashRegisterTable = () => {
+  const [fetch, setFetch] = useState(false);
+  const [noResults, setNoResults] = useState(false);
 
-  getActiveBills = () => {
-    this.props.getActiveBills().then(() => {
-      this.setState({
-        fetch: true
-      });
+  const bills = useSelector(state => state.cash.activeBills);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getActiveBills()).then(() => {
+      setFetch(true);
+      console.log(bills);
     });
-  };
+  }, []);
 
-  closeModal = () => {
-    this.setState({
-      open: true
-    });
-  };
+  // componentDidMount() {
+  //   this.getActiveBills();
+  // }
 
-  render() {
-    return (
-      <React.Fragment>
-        {this.state.fetch && this.props.activeBills.length ? (
-          <Table celled className="rtl text-center">
+  // getActiveBills = () => {
+  //   this.props.getActiveBills().then(() => {
+  //     this.setState({
+  //       fetch: true
+  //     });
+  //   });
+  // };
+
+  // closeModal = () => {
+  //   this.setState({
+  //     open: true
+  //   });
+  // };
+
+  return (
+    <React.Fragment>
+      {fetch && bills.length ? (
+        <Table celled className="rtl text-center">
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell colSpan="5" className="text-right">
+                لیست فاکتورهای فعال
+              </Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+
+          <React.Fragment>
             <Table.Header>
               <Table.Row>
-                <Table.HeaderCell colSpan="5" className="text-right">
-                  لیست فاکتورهای فعال
+                <Table.HeaderCell className="table-border-left">
+                  <TableLabel>1</TableLabel>
+                  موبایل خریدار
                 </Table.HeaderCell>
+                <Table.HeaderCell>
+                  <TableLabel>2</TableLabel>
+                  اسم فروشنده
+                </Table.HeaderCell>
+                <Table.HeaderCell>
+                  <TableLabel>3</TableLabel>
+                  مبلغ نهایی فاکتور
+                </Table.HeaderCell>
+                <Table.HeaderCell>
+                  <TableLabel>4</TableLabel>
+                  تاریخ فاکتور
+                </Table.HeaderCell>
+                <Table.HeaderCell>عملیات فاکتور</Table.HeaderCell>
               </Table.Row>
             </Table.Header>
 
-            <React.Fragment>
-              <Table.Header>
-                <Table.Row>
-                  <Table.HeaderCell className="table-border-left">
-                    <TableLabel>1</TableLabel>
-                    موبایل خریدار
-                  </Table.HeaderCell>
-                  <Table.HeaderCell>
-                    <TableLabel>2</TableLabel>
-                    اسم فروشنده
-                  </Table.HeaderCell>
-                  <Table.HeaderCell>
-                    <TableLabel>3</TableLabel>
-                    مبلغ نهایی فاکتور
-                  </Table.HeaderCell>
-                  <Table.HeaderCell>
-                    <TableLabel>4</TableLabel>
-                    تاریخ فاکتور
-                  </Table.HeaderCell>
-                  <Table.HeaderCell>عملیات فاکتور</Table.HeaderCell>
-                </Table.Row>
-              </Table.Header>
-
-              <Table.Body>
-                {this.props.activeBills.map(bill => {
-                  return (
-                    <Table.Row>
-                      <Table.Cell
-                        className="table-border-left"
-                        style={{ fontFamily: "arial" }}
-                      >
-                        <TableLabel>1</TableLabel>
-                        {bill.buyer.phone_number}
-                      </Table.Cell>
-                      <Table.Cell>
-                        <TableLabel>2</TableLabel>
-                        {bill.seller.first_name} {bill.seller.last_name}
-                      </Table.Cell>
-                      <Table.Cell
-                        style={{ fontFamily: "arial", fontWeight: "bold" }}
-                      >
-                        <TableLabel>3</TableLabel>
-                        {digitToComma(bill.final_price)}&nbsp;
-                        <span className="yekan">تومان</span>
-                      </Table.Cell>
-                      <Table.Cell style={{ fontFamily: "arial" }}>
-                        <TableLabel>4</TableLabel>
-                        {standardTimeToJalaali(
-                          convertToJalaali(bill.create_date)
-                        )}
-                      </Table.Cell>
-                      <Table.Cell>
-                        <Button
-                          icon="info"
-                          labelPosition="right"
-                          color="teal"
-                          content="مشاهده"
-                          className="yekan"
-                          onClick={() => {
-                            history.push(`/cashregister/${bill.pk}`);
-                          }}
-                        />
-                        <Button
-                          icon="delete"
-                          labelPosition="right"
-                          color="redلیست فاکتورهای ف"
-                          content="حذف"
-                          className="yekan"
-                          LoadingBar
-                          onClick={() => {
-                            this.props
-                              .deleteBill(bill.pk)
-                              .then(() => {
-                                toastr.success(
-                                  "حذف فاکتور با موفقیت انجام شد",
-                                  "فاکتور با موفقیت حذف گردید"
-                                );
-                                this.props.getActiveBills();
-                              })
-                              .catch(() => {
-                                toastr.error("عملیات حذف ناموفق بود");
-                              });
-                          }}
-                        />
-                      </Table.Cell>
-                    </Table.Row>
-                  );
-                })}
-              </Table.Body>
-            </React.Fragment>
-          </Table>
-        ) : null}
-        {!this.state.fetch ? <LoadingBar /> : null}
-        {this.state.fetch && !this.props.activeBills.length ? (
-          <NotFound />
-        ) : null}
-      </React.Fragment>
-    );
-  }
-}
-
-const mapStateToProps = state => {
-  return {
-    activeBills: state.cash.activeBills && state.cash.activeBills.results
-  };
+            <Table.Body>
+              {bills.map(bill => {
+                return (
+                  <Table.Row>
+                    <Table.Cell
+                      className="table-border-left"
+                      style={{ fontFamily: "arial" }}
+                    >
+                      <TableLabel>1</TableLabel>
+                      {bill.buyer.phone_number}
+                    </Table.Cell>
+                    <Table.Cell>
+                      <TableLabel>2</TableLabel>
+                      {bill.seller.first_name} {bill.seller.last_name}
+                    </Table.Cell>
+                    <Table.Cell
+                      style={{ fontFamily: "arial", fontWeight: "bold" }}
+                    >
+                      <TableLabel>3</TableLabel>
+                      {digitToComma(bill.final_price)}&nbsp;
+                      <span className="yekan">تومان</span>
+                    </Table.Cell>
+                    <Table.Cell style={{ fontFamily: "arial" }}>
+                      <TableLabel>4</TableLabel>
+                      {standardTimeToJalaali(
+                        convertToJalaali(bill.create_date)
+                      )}
+                    </Table.Cell>
+                    <Table.Cell>
+                      <Button
+                        icon="info"
+                        labelPosition="right"
+                        color="teal"
+                        content="مشاهده"
+                        className="yekan"
+                        onClick={() => {
+                          history.push(`/cashregister/${bill.pk}`);
+                        }}
+                      />
+                      <Button
+                        icon="delete"
+                        labelPosition="right"
+                        color="redلیست فاکتورهای ف"
+                        content="حذف"
+                        className="yekan"
+                        LoadingBar
+                        onClick={() => {
+                          deleteBill(bill.pk)
+                            .then(() => {
+                              toastr.success(
+                                "حذف فاکتور با موفقیت انجام شد",
+                                "فاکتور با موفقیت حذف گردید"
+                              );
+                              this.props.getActiveBills();
+                            })
+                            .catch(() => {
+                              toastr.error("عملیات حذف ناموفق بود");
+                            });
+                        }}
+                      />
+                    </Table.Cell>
+                  </Table.Row>
+                );
+              })}
+            </Table.Body>
+          </React.Fragment>
+        </Table>
+      ) : null}
+      {!fetch ? <LoadingBar /> : null}
+      {fetch && !bills.length ? <NotFound /> : null}
+    </React.Fragment>
+  );
 };
 
-export default connect(mapStateToProps, { getActiveBills, deleteBill })(
-  CashRegisterTable
-);
+// const mapStateToProps = state => {
+//   return {
+//     activeBills: state.cash.activeBills && state.cash.activeBills.results
+//   };
+// };
+
+export default CashRegisterTable;
--- frontend/src/components/cashRegister/index.js
+++ frontend/src/components/cashRegister/index.js
@@ -3,29 +3,27 @@ import { Container, Segment, Button } from "semantic-ui-react";
 import history from "../../history";
 import CashregisterTable from "./CashRegisterTable";
 
-class Cashregister extends React.Component {
-  render() {
-    return (
-      <Container>
-        <Segment stacked className="rtl">
-          <Button
-            className="yellow"
-            onClick={() => history.push("/daily-report/")}
-            color="teal"
-            content="مشاهده گزارش روزانه"
-            icon="print"
-            labelPosition="right"
-            className="yekan"
-            onClick={() => {
-              history.push("/daily-report/");
-            }}
-          />
-        </Segment>
+const Cashregister = () => {
+  return (
+    <Container>
+      <Segment stacked className="rtl">
+        <Button
+          className="yellow"
+          onClick={() => history.push("/daily-report/")}
+          color="teal"
+          content="مشاهده گزارش روزانه"
+          icon="print"
+          labelPosition="right"
+          className="yekan"
+          onClick={() => {
+            history.push("/daily-report/");
+          }}
+        />
+      </Segment>
 
-        <CashregisterTable />
-      </Container>
-    );
-  }
-}
+      <CashregisterTable />
+    </Container>
+  );
+};
 
 export default Cashregister;
