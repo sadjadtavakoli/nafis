448c1d6107530e7bae605ed92c863028befec386
Dier. Cohen <i.am.dier.cohen@gmail.com>
2020-03-12 12:15:33 +0330

    fix factor tab for remained bill

--- frontend/src/components/customers/FactorsTab.js
+++ frontend/src/components/customers/FactorsTab.js
@@ -37,10 +37,20 @@ class FactorsTab extends Component {
         fetch: true
       });
     });
-    this.props.getRemainedBills(this.props.passingPk);
   };
   handleToggleClick = () => {
-    this.setState({ remainedBillsToggle: !this.state.remainedBillsToggle });
+    this.setState(
+      { remainedBillsToggle: !this.state.remainedBillsToggle, fetch: false },
+      () => {
+        if (this.state.remainedBillsToggle === true) {
+          this.props.getRemainedBills(this.props.passingPk).then(res => {
+            this.setState({ fetch: true });
+          });
+        } else {
+          this.fetchData();
+        }
+      }
+    );
   };
 
   deleteBill = pk => {
@@ -108,8 +118,8 @@ class FactorsTab extends Component {
                   <Grid.Column verticalAlign="middle" width="2">
                     <h3 className="yekan text-right">
                       {this.state.remainedBillsToggle
-                        ? "فاکتور های باقی مانده"
-                        : "فاکتور ها"}
+                        ? "فاکتورهای‌باقی‌مانده"
+                        : "فاکتورها"}
                     </h3>
                   </Grid.Column>
                   <Grid.Column textAlign="right" verticalAlign="middle">
@@ -191,127 +201,129 @@ class FactorsTab extends Component {
           </Table.Row>
         </Table.Header>
         <Table.Body>
-          {bills.results.map((bill, index) => {
-            return (
-              <Table.Row key={bill.pk}>
-                <Table.Cell className="d-table-border" id="norm-latin">
-                  <TableLabel>1</TableLabel>
-                  {index + 1}
-                </Table.Cell>
-                <Table.Cell className="norm-latin">
-                  <TableLabel>2</TableLabel>
-                  <span>{bill.pk}</span>
-                </Table.Cell>
-                <Table.Cell className="norm-latin">
-                  <TableLabel>3</TableLabel>
-                  <span>{bill.buyer.phone_number}</span>
-                </Table.Cell>
-                <Table.Cell className="norm-latin">
-                  <TableLabel>4</TableLabel>
-                  {bill.close_date ? (
-                    <span>
-                      {standardTimeToJalaali(convertToJalaali(bill.close_date))}
-                    </span>
-                  ) : (
-                    <span className="yekan">ندارد</span>
-                  )}
-                </Table.Cell>
-                <Table.Cell id="norm-latin">
-                  <TableLabel>5</TableLabel>
-                  <span id="norm-latin">{bill.items.length}</span>
-                </Table.Cell>
-                <Table.Cell className="yekan">
-                  <TableLabel>6</TableLabel>
-                  <span>
-                    {bill.seller.ffirst_name}&nbsp;{bill.seller.last_name}
-                  </span>
-                </Table.Cell>
-                <Table.Cell className="yekan">
-                  <TableLabel>7</TableLabel>
-                  <span>
-                    {bill.closande ? (
-                      <span>
-                        {bill.closande.first_name}&nbsp;
-                        {bill.closande.last_name}
-                      </span>
-                    ) : (
-                      "موجود نمی باشد"
-                    )}
-                  </span>
-                </Table.Cell>
-                <Table.Cell className="norm-latin">
-                  <TableLabel>8</TableLabel>
-                  <span>{digitToComma(bill.price)}</span>&nbsp;
-                  <span className="yekan">تومان</span>
-                </Table.Cell>
-                <Table.Cell className="norm-latin">
-                  <TableLabel>9</TableLabel>
-                  <span>{digitToComma(bill.discount)}</span>&nbsp;
-                  <span className="yekan">تومان</span>
-                </Table.Cell>
-                <Table.Cell className="norm-latin">
-                  <TableLabel>10</TableLabel>
-                  <span>{digitToComma(bill.final_price)}</span>
-                  <span className="yekan"> تومان</span>
-                </Table.Cell>
-                {this.state.remainedBillsToggle ? (
+          {this.state.fetch &&
+            bills.results.map((bill, index) => {
+              return (
+                <Table.Row key={bill.pk}>
+                  <Table.Cell className="d-table-border" id="norm-latin">
+                    <TableLabel>1</TableLabel>
+                    {index + 1}
+                  </Table.Cell>
                   <Table.Cell className="norm-latin">
-                    <TableLabel>11</TableLabel>
-                    <span>{digitToComma(bill.paid)}</span>
+                    <TableLabel>2</TableLabel>
+                    <span>{bill.pk}</span>
+                  </Table.Cell>
+                  <Table.Cell className="norm-latin">
+                    <TableLabel>3</TableLabel>
+                    <span>{bill.buyer.phone_number}</span>
+                  </Table.Cell>
+                  <Table.Cell className="norm-latin">
+                    <TableLabel>4</TableLabel>
+                    {bill.close_date ? (
+                      <span>{convertToJalaali(bill.close_date)}</span>
+                    ) : (
+                      <span className="yekan">ندارد</span>
+                    )}
+                  </Table.Cell>
+                  <Table.Cell id="norm-latin">
+                    <TableLabel>5</TableLabel>
+                    <span id="norm-latin">{bill.items.length}</span>
+                  </Table.Cell>
+                  <Table.Cell className="yekan">
+                    <TableLabel>6</TableLabel>
+                    <span>
+                      {bill.seller.ffirst_name}&nbsp;{bill.seller.last_name}
+                    </span>
+                  </Table.Cell>
+                  <Table.Cell className="yekan">
+                    <TableLabel>7</TableLabel>
+                    <span>
+                      {bill.closande ? (
+                        <span>
+                          {bill.closande.first_name}&nbsp;
+                          {bill.closande.last_name}
+                        </span>
+                      ) : (
+                        "موجود نمی باشد"
+                      )}
+                    </span>
+                  </Table.Cell>
+                  <Table.Cell className="norm-latin">
+                    <TableLabel>8</TableLabel>
+                    <span>{digitToComma(bill.price)}</span>&nbsp;
+                    <span className="yekan">تومان</span>
+                  </Table.Cell>
+                  <Table.Cell className="norm-latin">
+                    <TableLabel>9</TableLabel>
+                    <span>{digitToComma(bill.discount)}</span>&nbsp;
+                    <span className="yekan">تومان</span>
+                  </Table.Cell>
+                  <Table.Cell className="norm-latin">
+                    <TableLabel>10</TableLabel>
+                    <span>{digitToComma(bill.final_price)}</span>
                     <span className="yekan"> تومان</span>
                   </Table.Cell>
-                ) : null}
-                {this.state.remainedBillsToggle ? (
-                  <Table.Cell className="norm-latin">
-                    <TableLabel>12</TableLabel>
-                    <span>{bill.final_price - bill.paid}</span>
+                  {this.state.remainedBillsToggle ? (
+                    <Table.Cell className="norm-latin">
+                      <TableLabel>11</TableLabel>
+                      <span>{digitToComma(bill.paid)}</span>
+                      <span className="yekan"> تومان</span>
+                    </Table.Cell>
+                  ) : null}
+                  {this.state.remainedBillsToggle ? (
+                    <Table.Cell className="norm-latin">
+                      <TableLabel>12</TableLabel>
+                      <span>{bill.final_price - bill.paid}</span>
+                    </Table.Cell>
+                  ) : null}
+                  <Table.Cell className="yekan">
+                    <TableLabel>
+                      {this.state.remainedBillsToggle ? "13" : "11"}
+                    </TableLabel>
+                    <span>{bill.status === "done" ? "تسویه" : null}</span>
+                    <span>{bill.status === "active" ? "باز" : null}</span>
+                    <span>{bill.status === "remained" ? "مانده" : null}</span>
                   </Table.Cell>
-                ) : null}
-                <Table.Cell className="yekan">
-                  <TableLabel>
-                    {this.state.remainedBillsToggle ? "13" : "11"}
-                  </TableLabel>
-                  <span>{bill.status === "done" ? "تسویه" : null}</span>
-                  <span>{bill.status === "active" ? "باز" : null}</span>
-                  <span>{bill.status === "remained" ? "مانده" : null}</span>
-                </Table.Cell>
-                <Table.Cell className="yekan">
-                  <Button
-                    className="yekan"
-                    color="teal"
-                    size="mini"
-                    onClick={() => {
-                      history.push(`/cashregister/${bill.pk}`);
-                    }}
-                  >
-                    مشاهده
-                  </Button>
-                  <br />
-                  <Button
-                    className="yekan"
-                    color="red"
-                    style={{ marginTop: "3px" }}
-                    size="mini"
-                    onClick={() => this.deleteBill(bill.pk)}
-                  >
-                    حذف کل فاکتور
-                  </Button>
-                  <br />
-                  <Button
-                    className="yekan"
-                    color="teal"
-                    style={{ marginTop: "3px" }}
-                    size="mini"
-                    onClick={() => {
-                      history.push(`/factor/${bill.pk}/print`);
-                    }}
-                  >
-                    چاپ فاکتور
-                  </Button>
-                </Table.Cell>
-              </Table.Row>
-            );
-          })}
+                  <Table.Cell className="yekan">
+                    <Button
+                      className="yekan w-100"
+                      color="teal"
+                      size="mini"
+                      onClick={() => {
+                        history.push(`/cashregister/${bill.pk}`);
+                      }}
+                      content="مشاهده"
+                      icon="info"
+                      labelPosition="right"
+                    />
+                    <br />
+                    <Button
+                      className="yekan w-100"
+                      color="red"
+                      style={{ marginTop: "3px" }}
+                      size="mini"
+                      onClick={() => this.deleteBill(bill.pk)}
+                      content="حذف‌فاکتور"
+                      icon="trash"
+                      labelPosition="right"
+                    />
+                    <br />
+                    <Button
+                      className="yekan w-100"
+                      color="teal"
+                      style={{ marginTop: "3px" }}
+                      size="mini"
+                      onClick={() => {
+                        history.push(`/factor/${bill.pk}/print`);
+                      }}
+                      content="چاپ‌فاکتور"
+                      icon="print"
+                      labelPosition="right"
+                    />
+                  </Table.Cell>
+                </Table.Row>
+              );
+            })}
         </Table.Body>
         {this.props.allBills && this.props.allBills.count > 25 ? (
           <Table.Footer>
